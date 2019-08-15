29ae078816483fc3c4db99625025be11fb99d4ae
i_am@nulleekh.com <i_am@nulleekh.com>
2019-08-15 14:12:28 +0900

    Make code look better

--- src/2048/main.cpp
+++ src/2048/main.cpp
@@ -2,6 +2,7 @@
 #include<time.h>
 #include<conio.h>
 #include<windows.h>
+
 #define BLACK 0
 #define BLUE 1
 #define GREEN 2
@@ -18,6 +19,7 @@
 #define LIGHTMAGENTA 13
 #define YELLOW 14
 #define WHITE 15
+
 #define LEFT 75
 #define RIGHT 77
 #define UP 72
@@ -41,14 +43,14 @@ void TextColor(int foreground, int background);
 
 
 int main (void) {
-    int i, j, r;
-    int key;
-    int move;
+    int i, j, r, key, move;
+
     Countdown();
     AD();
     NewNum();
     NewNum();
     Draw();
+
     while(1) {
         key=getch();
         if(key==0xE0||key==0) {
@@ -67,6 +69,7 @@ int main (void) {
                                     board[i][r-1]+=10000;
                                     score+=2*(board[i][r]);
                                 }
+
                                 board[i][r]=0;
                                 move++;
                             }
@@ -86,6 +89,7 @@ int main (void) {
                                     board[i][r+1]+=10000;
                                     score+=2*(board[i][r]);
                                 }
+
                                 board[i][r]=0;
                                 move++;
                             }
@@ -105,6 +109,7 @@ int main (void) {
                                     board[r-1][j]+=10000;
                                     score+=2*(board[r][j]);
                                 }
+
                                 board[r][j]=0;
                                 move++;
                             }
@@ -124,6 +129,7 @@ int main (void) {
                                     board[r+1][j]+=10000;
                                     score+=2*(board[r][j]);
                                 }
+
                                 board[r][j]=0;
                                 move++;
                             }
@@ -132,11 +138,10 @@ int main (void) {
                     break;
             }
         }
-        for (i=0;i<4;i++) {
-            for (j=0;j<4;j++) {
+        for (i=0;i<4;i++)
+            for (j=0;j<4;j++)
                 if(board[i][j]>10000) board[i][j]-=10000;
-            }
-        }
+
         if(move>0) {
             NewNum();
             Draw();
@@ -148,13 +153,14 @@ int main (void) {
 void Wait(int seconds) {
     clock_t endwait;
     endwait = clock() + seconds * CLOCKS_PER_SEC;
+
     while (clock() < endwait);
 }
 
 void Countdown(void) {
     int n;
-    printf("  Starting countdown...\n");
-    printf(" ");
+
+    printf("  Starting countdown...\n ");
     for (n = 10; n > 0; n--) {
         printf("%d ", n)
         Wait(1);
@@ -163,6 +169,7 @@ void Countdown(void) {
 
 void AD(void) {
     printf("\n\n\n");
+
     TextColor(YELLOW, BLACK); {
         printf("   ■■■■   ■■■■      ■    ■■■■ \n");
         printf("         ■   ■    ■    ■■    ■    ■ \n");
@@ -170,14 +177,17 @@ void AD(void) {
         printf("   ■         ■    ■  ■■■■  ■    ■ \n");
         printf("   ■■■■   ■■■■      ■    ■■■■ \n");
     }
+
     Wait(1);
 }
 
 void Draw(void) {
     int i, j;
+
     TextColor(WHITE, BLACK);
     system("cls");
     printf("\n\n");
+
     for (i=0;i<4;i++) {
         for (j=0;j<4;j++) {
             if (board[i][j] == 0) printf("    .");
@@ -185,41 +195,44 @@ void Draw(void) {
         }
         printf("\n");
     }
-    printf("\n");
-    printf("\n");
+
+    printf("\n\n");
     TextColor(GREEN, BLACK);
     printf("   Score : %d \n", score);
 }
 
 void NewNum(void) {
-    int i, j;
-    int cnt;
+    int i, j, cnt;
     int* p0[16]= {0};
-    for (i=0, cnt=0;i<4;i++) {
+
+    for (i=0, cnt=0;i<4;i++)
         for (j=0;j<4;j++)
             if(board[i][j] == 0) {
                 p0[cnt]=&board[i][j];
                 cnt++;
-            }
-    }
+
     *p0[rand()%(cnt)] =(rand()%100<80)?2:4;
 }
 
 void CheckGameOver(void) {
     int i, j;
+
     for (i=0;i<4;i++)
         for (j=0;j<4;j++)
             if(board[i][j]==0) return;
+
     for (i=0;i<3;i++)
         for (j=0;j<3;j++)
             if(board[i][j]==board[i+1][j]||board[i][j]==board[i][j+1]) return;
+
     for (i=0;i<3;i++) if(board[i][3]==board[i+1][3]) return;
+
     for (j=0;j<3;j++) if(board[3][j]==board[3][j+1]) return;
+
     Wait(1);
     system("cls");
     TextColor(RED, BLACK);
-    printf("\n");
-    printf("  Game Over..");
+    printf("\n  Game Over..");
     exit(0);
 }
 
